eb1e26a5dc311e99945d136ad02f1ae91de2e2eb
Sushil Magar <sushilmagar@Sushils-Air.lan>
2022-07-31 17:13:02 -0700

    added create react app

--- cypress/e2e/simple-spec.cy.js
+++
@@ -1,9 +0,0 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('websites.godaddy.com');
-    cy.get('#login-link', { timeout: 10000 }).click();
-    cy.get('#label-username').type('n5z');
-    cy.get('#label-password').type('Fullerton2020!');
-    cy.get('#submitBtn').click();
-  })
-})
