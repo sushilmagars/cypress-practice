4a1fcfee3d21be1f458aff160b1f3c8034ccbf48
Sushil Magar <sushilmagar@Sushils-MBP-2.lan>
2022-09-27 07:59:11 -0700

    Form updates

--- cypress/component/FormComponent.cy.js
+++ cypress/component/FormComponent.cy.js
@@ -1,9 +1,13 @@
 import Form from '../../src/components/Form';
 
 describe('Form component testing', () => {
-  it('playground', () => {
+  it('should not allow user to submit form with invalid email', () => {
     cy.mount(<Form />);
-    cy.get('[data-aid=USER_NAME]').type('Sushil');
-    cy.get('[data-aid=SUBMIT_FORM]').should('be.visible');
+    cy.get('[data-aid=USER_NAME]').type('Sushil M');
+    cy.get('[data-aid=USER_EMAIL]').type('sushil');
+    cy.get('[data-aid=SUBMIT_FORM]')
+      .should('be.visible')
+      .click();
+    cy.get('.registration-form').contains('please');
   });
-})
+});
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Button(props) {
+	const [ username, setUsername ] = useState('');
+	const [ email, setEmail ] = useState('');
 	const style = {
 		padding: '50px',
 		margin: '15px'
 	}
-	return (<form style={style}>
-		<label>
-			Name:
-			<input type='text' name='name' data-aid='USER_NAME'/>
-			</label>
-		<input type='submit' value='Submit' data-aid='SUBMIT_FORM'/>
-	</form>);
+
+	const handleNameChange = (event) => {
+		setUsername(event.target.value);
+	}
+
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	}
+
+	return (<>
+		<form 
+			class='registration-form'
+			style={style}>
+			<input
+				type='text'
+				name='username'
+				value={username}
+				data-aid='USER_NAME'
+				onChange={handleNameChange}
+			/>
+			<input
+				type='email'
+				name='email'
+				value={email}
+				data-aid='USER_EMAIL'
+				onChange={handleEmailChange}
+				required
+			/>
+			<input
+				type='submit'
+				value='Submit'
+				data-aid='SUBMIT_FORM'
+			/>
+		</form>
+	</>);
 }
